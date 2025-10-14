bfd2d40db3c3b4717b634574653b6dd860c8954b
kiyoshi-honda <kiyoshi.honda@oit.ac.jp>
2025-10-14 16:52:33 +0900

    ログインの実行ができることを確認

--- janken/src/main/java/oit/is/z9999/kaizi/janken/security/SecurityConfig.java
+++ janken/src/main/java/oit/is/z9999/kaizi/janken/security/SecurityConfig.java
@@ -1,13 +1,17 @@
 package oit.is.z9999.kaizi.janken.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
   /**
    * Spring Securityのフィルタチェーン設定
@@ -16,10 +20,10 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .authorizeHttpRequests(authz -> authz
-            .requestMatchers("/janken", "/janken/result").authenticated()
+            .requestMatchers("/", "/janken", "/janken/result").authenticated()
             .anyRequest().permitAll())
         .formLogin(form -> form
-            .loginPage("/login").permitAll());
+            .permitAll());
     return http.build();
   }
 
